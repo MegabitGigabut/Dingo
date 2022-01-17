34d392582ce0a45346e397fba420724dc3b221a7
Kloend <knura@deloittece.com>
2022-01-17 14:48:00 +0100

    change

--- force-app/main/default/lwc/accountcomp/accountcomp.js
+++ force-app/main/default/lwc/accountcomp/accountcomp.js
@@ -1,6 +1,6 @@
 import { LightningElement, wire,api } from 'lwc';
 import getRandomAccs from '@salesforce/apex/RandomAccCreator.getRandomAccs';
-//I'm in branch add-ons
+//I'm in branch add-on
 
 const columns = [
     {label:'Account Name', fieldName:'Name', type:'Name'},
